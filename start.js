847f94ca4921986ead0533d79fe564b59e29f572
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2022-01-10 11:39:53 +0900

    Add files via upload

---
+++ start.js
@@ -0,0 +1,11 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+
+function begin() {
+    main.style.webkitAnmation = "fadeOut 1s";
+    main.style.anmation = "fadeOut 1s";
+    qna.style.webkitAnmation = "fadeIn 1s";
+    qna.style.anmation = "fadeIn 1s";
+    //main.style.display = "none";
+    //qna.style.display = "block";
+}
