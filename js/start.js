975a9fd99131356b8e21914042359f0371900fa1
NAYOUNG <yny3533@naver.com>
2022-01-30 23:47:27 +0900

    JJINMAK

--- js/start.js
+++ js/start.js
@@ -134,17 +134,3 @@ function begin() {
         goNext(n);
     }, 300);
 }
-
-function moveHome() {
-    result.style.WebkitAnimation = "fadeOut 0.8s";
-    result.style.animation = "fadeOut 0.8s";
-    setTimeout(() => {
-        main.style.WebkitAnimation = "fadeIn 0.8s";
-        main.style.animation = "fadeIn 0.8s";
-        setTimeout(() => {
-            result.style.display = "none";
-            main.style.display = "block";
-        }, 300)
-    }
-    )
-}
